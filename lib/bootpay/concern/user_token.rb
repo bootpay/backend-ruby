f00c5ab915728a8a44ed90811a3a6967a1601424
gosomi <gosomi@udid.co.kr>
2022-02-08 11:27:59 +0900

    username으로 키 변경

--- lib/bootpay/concern/user_token.rb
+++ lib/bootpay/concern/user_token.rb
@@ -5,17 +5,17 @@ module Bootpay::Concern::UserToken
     # User Token 정보를 가져온다
     # Comment by Gosomi
     # Date: 2022-02-03
-    def request_user_token(user_id:, email: nil, name: nil, gender: -1, birth: nil, phone: nil)
+    def request_user_token(user_id:, email: nil, username: nil, gender: -1, birth: nil, phone: nil)
       request(
         method:  :post,
         uri:     'request/user/token',
         payload: {
-          user_id: user_id,
-          email:   email,
-          name:    name,
-          gender:  gender,
-          birth:   birth,
-          phone:   phone
+          user_id:  user_id,
+          email:    email,
+          username: username,
+          gender:   gender,
+          birth:    birth,
+          phone:    phone
         }
       )
     end
