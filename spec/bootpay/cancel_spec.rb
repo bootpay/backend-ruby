e3e6587d16135abaf819af32cb8b522f8a9906a2
gosomi <gosomi@udid.co.kr>
2022-07-07 21:50:44 +0900

    gitignore 추가

--- spec/bootpay/cancel_spec.rb
+++ spec/bootpay/cancel_spec.rb
@@ -2,18 +2,23 @@
 
 RSpec.describe Bootpay::RestClient do
   it "cancel payment" do
+    # api = Bootpay::RestClient.new(
+    #   application_id: '59bfc738e13f337dbd6ca48a',
+    #   private_key:    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+    #   mode:           'development'
+    # )
     api = Bootpay::RestClient.new(
-      application_id: '59bfc738e13f337dbd6ca48a',
-      private_key:    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
-      mode:           'development'
+      application_id: '59b731f084382614ebf72215',
+      private_key:    'WwDv0UjfwFa04wYG0LJZZv1xwraQnlhnHE375n52X0U=',
+    # mode:           'development'
     )
     if api.request_access_token.success?
       response = api.cancel_payment(
-        receipt_id:      "624a56111fc19202e4746df2",
+        receipt_id:      "62b1c34e778102001f4a4fa8",
         cancel_price:    1000,
+        cancel_tax_free: 0,
         cancel_username: 'test_user',
         cancel_message:  'test_message',
-
       )
       print response.data.to_json
     end
