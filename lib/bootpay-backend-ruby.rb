7ab10624edc0f654454b2974f00835dd66141d27
gosomi <gosomi@udid.co.kr>
2023-05-26 16:40:22 +0900

    version 변겨

--- lib/bootpay-backend-ruby.rb
+++ lib/bootpay-backend-ruby.rb
@@ -17,7 +17,7 @@ module Bootpay
         production:  'https://api.bootpay.co.kr/v2'
       }
 
-    SDK_VERSION = '4.2.0'
+    SDK_VERSION = '4.2.1'
 
     def initialize(application_id:, private_key:, mode: 'production')
       @application_id = application_id
