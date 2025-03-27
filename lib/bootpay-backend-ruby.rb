a9de00e541e5abf2aacb0a0a74b30df1828f5bb1
rupy1014 <rupy1014@gmail.com>
2025-03-27 10:43:10 +0900

    interface added

---
+++ lib/bootpay-backend-ruby.rb
@@ -0,0 +1,4 @@
+# lib/bootpay.rb
+require_relative "bootpay/bootpay-rest-client"
+require_relative "bootpay_storage/bootpay-storage-rest-client"
+require_relative "bootpay_store/bootpay-store-rest-client"
--- lib/version.rb
+++ lib/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module Bootpay
-  V2_VERSION = "3.0.0-alpha.2"
+  V2_VERSION = "3.0.0-alpha.3"
 end
