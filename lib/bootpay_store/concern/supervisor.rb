df4cd281e99d5e815680247214b1eeaa49a0e9be
gosomi <gosomi@bootpay.co.kr>
2025-05-12 08:31:04 +0900

    order 결제 취소 파라메터 추가

--- lib/bootpay_store/concern/supervisor.rb
+++ lib/bootpay_store/concern/supervisor.rb
@@ -6,7 +6,8 @@ module BootpayStore::Concern::Supervisor
     # Comment by GOSOMI
     # @date: 2025-04-04
     def supervisor_request_order_cancel(idempotency_key: nil, cancel_id: nil, order_number:, cancel_products: nil, cancel_price: nil,
-                                        cancel_tax_free_price: 0, cancel_requester: '시스템', cancel_message: '요청취소', cancel_immediately: false)
+                                        cancel_tax_free_price: 0, cancel_requester: '시스템', cancel_message: '요청취소', cancel_immediately: false,
+                                        cancel_order_subscription_bills: nil)
       request(
         uri:     'role/supervisor/order/cancel',
         headers: {
@@ -16,13 +17,14 @@ module BootpayStore::Concern::Supervisor
                  {
                    order_number:              order_number,
                    request_cancel_parameters: {
-                                                cancel_id:             cancel_id,
-                                                cancel_products:       cancel_products,
-                                                cancel_price:          cancel_price,
-                                                cancel_tax_free_price: cancel_tax_free_price,
-                                                cancel_requester:      cancel_requester,
-                                                cancel_message:        cancel_message,
-                                                cancel_immediately:    cancel_immediately
+                                                cancel_id:                       cancel_id,
+                                                cancel_products:                 cancel_products,
+                                                cancel_order_subscription_bills: cancel_order_subscription_bills,
+                                                cancel_price:                    cancel_price,
+                                                cancel_tax_free_price:           cancel_tax_free_price,
+                                                cancel_requester:                cancel_requester,
+                                                cancel_message:                  cancel_message,
+                                                cancel_immediately:              cancel_immediately
                                               }.compact
                  }.compact
       )
