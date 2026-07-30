16d00e2ec59a02f6197b44f51269fd7ed69804d8
alfredhot <alfredhot@gmail.com>
2026-07-30 09:23:34 +0900

    storage 이미지 삭제 API 추가 (image_destroy)

--- lib/bootpay_storage/concern/image.rb
+++ lib/bootpay_storage/concern/image.rb
@@ -14,5 +14,17 @@ module BootpayStorage::Concern::Image
       )
     end
 
+    # 업로드한 이미지를 URL 로 삭제한다.
+    # 업로드 응답이 URL 만 돌려주므로(내부 식별자 미노출) 삭제도 URL 을 키로 받는다.
+    # Comment by Claude (alfred)
+    # @date: 2026-07-28
+    def image_destroy(url:)
+      request(
+        method:  :delete,
+        uri:     'images/by_url',
+        payload: { url: url }
+      )
+    end
+
   end
 end
